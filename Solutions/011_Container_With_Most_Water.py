7acdb2b21e21b404f2ef9292eacaa5178b37cc0f
Michael Xiang <xiangt@bc.edu>
2015-07-30 23:43:05 -0400

    update 2 solutions

---
+++ Solutions/011_Container_With_Most_Water.py
@@ -0,0 +1,10 @@
+class Solution:
+    # @param {integer[]} height
+    # @return {integer}
+    def maxArea(self, height):
+        maxV = 0
+        for i in range(1,len(height)):
+        	for j in range(i + 1, len(height) + 1):
+        		if j == len(height) or height[j - 1] < height[j]:
+        			maxV = max(maxV, (j - i) * (height[j - 1] + height[i - 1]) / 2)
+        return maxV
--- Solutions/189_Rotate_Array.py
+++ Solutions/189_Rotate_Array.py
@@ -3,7 +3,9 @@ class Solution:
 # @param {integer} k
 # @return {void} Do not return anything, modify nums in-place instead.
 	def rotate(self, nums, k):
-		if k != 0:
+		if k == 1:
+			self.swap(nums[0],nums[-1])
+		elif k != 0:
 			for i in range(len(nums) / k):
 				for j in range(k):
 					self.swap(nums[i*k +j], nums[-k + j])
