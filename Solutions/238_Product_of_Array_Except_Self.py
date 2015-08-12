35eaf15611f735501fcababec3faea9f1255c7cb
Michael Xiang <xiangt@bc.edu>
2015-08-11 23:24:42 -0400

    add solutions

--- Solutions/238_Product_of_Array_Except_Self.py
+++ Solutions/238_Product_of_Array_Except_Self.py
@@ -4,20 +4,20 @@ class Solution:
     def productExceptSelf(self, nums):
         product, numOfZero = self.getProduct(nums)
         for i in range(len(nums)):
-        	if numOfZero == 0:
+        	if numOfZero == 0:       #CASE 1. NO ZERO IN LIST
         		nums[i] = product / nums[i]
-        	elif numOfZero== 1:
+        	elif numOfZero== 1:      #CASE 2. ONE ZERO IN LIST
         		if nums[i] == 0:
         			nums[i] = product
-        		else:
+        		else:               
         			nums[i] = 0
-        	else:
+        	else:                    #CASE 3. MORE THAN ONE ZEROES IN LIST
         		nums[i] = 0
         return nums
 
     def getProduct(self, nums):
     	product = 1  #product of all non zero numbers
-    	numOfZero = 0 
+    	numOfZero = 0 # Number of zeroes
     	for i in nums:
     		if i != 0:
     			product *= i
